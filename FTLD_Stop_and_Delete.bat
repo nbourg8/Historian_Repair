0c32863586eb2be426471af9643ab096278fa2d6
nbourg8 <nbourg8@users.noreply.github.com>
2017-08-26 14:35:11 -0500

    Add files via upload
    Check for existing files before making new folders.
    <addressing modular vs not modular>

--- FTLD_Stop_and_Delete.bat
+++ FTLD_Stop_and_Delete.bat
@@ -15,28 +15,19 @@ echo.>>%LogFile%
 net stop pibufss
 
 echo.Deleting:>>%LogFile%
-forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\interfaces\ /m ftld* /d -%1% /c "cmd /c echo @file">>%LogFile%
-forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\interfaces\ /m ftld* /d -%1% /c "cmd /c echo @file">>%LogFile%
+if exist c:\progra~1\rockwe~1\factor~1\pipc\interfaces\ftld* (forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\interfaces\ /m ftld* /d -%1% /c "cmd /c echo @file">>%LogFile% && del /q /f c:\progra~1\rockwe~1\factor~1\pipc\interfaces\ftld*)
+if exist c:\progra~2\rockwe~1\factor~2\pipc\interfaces\ftld* (forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\interfaces\ /m ftld* /d -%1% /c "cmd /c echo @file">>%LogFile% && del /q /f c:\progra~2\rockwe~1\factor~2\pipc\interfaces\ftld*)
 
-del /q /f c:\progra~1\rockwe~1\factor~1\pipc\interfaces\ftld*
-del /q /f c:\progra~2\rockwe~1\factor~2\pipc\interfaces\ftld*
 echo.>>%LogFile%
 
-if not exist "c:\progra~1\rockwe~1\factor~1\pipc\pibufq_Backup" mkdir "c:\progra~1\rockwe~1\factor~1\pipc\pibufq_Backup"
-if not exist "c:\progra~2\rockwe~1\factor~2\pipc\pibufq_Backup" mkdir "c:\progra~2\rockwe~1\factor~2\pipc\pibufq_Backup"
+if exist c:\progra~1\rockwe~1\factor~1\pipc\dat\pibufq_* (if not exist c:\progra~1\rockwe~1\factor~1\pipc\pibufq_Backup mkdir c:\progra~1\rockwe~1\factor~1\pipc\pibufq_Backup)
+if exist c:\progra~2\rockwe~1\factor~2\pipc\dat\pibufq_* (if not exist c:\progra~2\rockwe~1\factor~2\pipc\pibufq_Backup mkdir c:\progra~2\rockwe~1\factor~2\pipc\pibufq_Backup)
 
 echo.Deleting:>>%LogFile%
-forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat\ /m pibufq_* /d -%1% /c "cmd /c echo @file">>%LogFile%
-forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat\ /m pibufq_* /d -%1% /c "cmd /c echo @file">>%LogFile%
-
-forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat /m pibufq_* /d -%1% /c "cmd /c del @file"
-forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\pibufq_Backup /m pibufq_* /c "cmd /c del @file"
-forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat /m pibufq_* /d -%1% /c "cmd /c del @file"
-forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\pibufq_Backup /m pibufq_* /c "cmd /c del @file"
+if exist c:\progra~1\rockwe~1\factor~1\pipc\dat\pibufq_* (forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat\ /m pibufq_* /d -%1% /c "cmd /c echo @file">>%LogFile% && forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat /m pibufq_* /d -%1% /c "cmd /c del @file" && forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\pibufq_Backup /m pibufq_* /c "cmd /c del @file")
+if exist c:\progra~2\rockwe~1\factor~2\pipc\dat\pibufq_* (forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat\ /m pibufq_* /d -%1% /c "cmd /c echo @file">>%LogFile% && forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat /m pibufq_* /d -%1% /c "cmd /c del @file" && forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\pibufq_Backup /m pibufq_* /c "cmd /c del @file")
 echo.>>%LogFile%
 
 echo.Moving to ..\pibufq_Backup:>>%LogFile%
-forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat\ /m pibufq_* /c "cmd /c echo @file">>%LogFile%
-forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat\ /m pibufq_* /c "cmd /c echo @file">>%LogFile%
-forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat /m pibufq_* /c "cmd /c move @file ..\pibufq_Backup\@file"
-forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat /m pibufq_* /c "cmd /c move @file ..\pibufq_Backup\@file"
+if exist c:\progra~1\rockwe~1\factor~1\pipc\dat\pibufq_* (forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat\ /m pibufq_* /c "cmd /c echo @file">>%LogFile% && forfiles /p c:\progra~1\rockwe~1\factor~1\pipc\dat\ /m pibufq_* /c "cmd /c move @file ..\pibufq_Backup\@file")
+if exist c:\progra~2\rockwe~1\factor~2\pipc\dat\pibufq_* (forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat\ /m pibufq_* /c "cmd /c echo @file">>%LogFile% && forfiles /p c:\progra~2\rockwe~1\factor~2\pipc\dat\ /m pibufq_* /c "cmd /c move @file ..\pibufq_Backup\@file")
