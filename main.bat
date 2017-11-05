7b42a3264604a2c445d3d3d92a74af7475a349e5
nbourg8 <nbourg8@users.noreply.github.com>
2017-11-05 13:39:19 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -18,35 +18,35 @@ if "%1"=="" (set /p daysback="Enter how many days to keep: ") else (set daysback
 echo.Keeping PIBUFQ_* and PIMQ* Files Newer than %daysback% days old>>%LogFile%
 
 net use x: \\192.168.1.13\C$
-psexec \\192.168.1.13 -c resources\FTLD_Stop_and_Delete.bat %daysback%
+psexec \\192.168.1.13 -c -f resources\FTLD_Stop_and_Delete.bat %daysback%
 copy x:\HISTREP.txt %~dp0
 copy /b %LogFile%+HISTREP.txt %LogFile%
 del x:\HISTREP.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.20\C$
-psexec \\192.168.1.20 -c resources\FTLD_Stop_and_Delete.bat %daysback%
+psexec \\192.168.1.20 -c -f resources\FTLD_Stop_and_Delete.bat %daysback%
 copy x:\HISTREP.txt %~dp0
 copy /b %LogFile%+HISTREP.txt %LogFile%
 del x:\HISTREP.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.14\C$
-psexec \\192.168.1.14 -c resources\Buffer_Repair.bat %daysback%
+psexec \\192.168.1.14 -c -f resources\Buffer_Repair.bat %daysback%
 copy x:\HISTREP.txt %~dp0
 copy /b %LogFile%+HISTREP.txt %LogFile%
 del x:\HISTREP.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.13\C$
-psexec \\192.168.1.13 -c resources\FTLD_Start.bat
+psexec \\192.168.1.13 -c -f resources\FTLD_Start.bat
 copy x:\HISTREP.txt %~dp0
 copy /b %LogFile%+HISTREP.txt %LogFile%
 del x:\HISTREP.txt
 net use x: /delete /y
 
 net use x: \\192.168.1.20\C$
-psexec \\192.168.1.20 -c resources\FTLD_Start.bat
+psexec \\192.168.1.20 -c -f resources\FTLD_Start.bat
 copy x:\HISTREP.txt %~dp0
 copy /b %LogFile%+HISTREP.txt %LogFile%
 del x:\HISTREP.txt
